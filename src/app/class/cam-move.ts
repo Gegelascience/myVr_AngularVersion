01dca4d8db32623ef2b949e8f8e7fd1991c0f47b
gegelascience <remi.verchere2@gmail.com>
2019-02-11 17:44:25 +0100

    code prettier

--- src/app/class/cam-move.ts
+++ src/app/class/cam-move.ts
@@ -1,30 +1,30 @@
 export class CamMove {
-    constructor(AFRAME:any){
-        AFRAME.registerComponent('cam_move', {
-            init:function(){
-            },
-          
-            tick: function (time, timeDelta) {
-              // Do something on every scene tick or frame.
-              var state = time % 62000;
-              var pos = this.el.getAttribute("position");
-              if (state <= 5250) {  
-                pos.x+= 20*timeDelta/5250;
-                this.el.setAttribute("position",pos);
-              } else if (state <= 25750) {
-                pos.z-= 128*timeDelta/20500;
-                this.el.setAttribute("position",pos);
-              } else if (state <= 36250) {
-                pos.x-= 40*timeDelta/10500;
-                this.el.setAttribute("position",pos);
-              } else if(state <= 56750){
-                pos.z+= 128*timeDelta/20500;
-                this.el.setAttribute("position",pos);
-              }else{
-                pos.x+= 20*timeDelta/5250;
-                this.el.setAttribute("position",pos);
-              } 
-            }
-        })
-    }
+  constructor(AFRAME: any) {
+    AFRAME.registerComponent('cam_move', {
+      init: function () {
+      },
+
+      tick: function (time, timeDelta) {
+        // Do something on every scene tick or frame.
+        const state = time % 62000;
+        const pos = this.el.getAttribute('position');
+        if (state <= 5250) {
+          pos.x += 20 * timeDelta / 5250;
+          this.el.setAttribute('position', pos);
+        } else if (state <= 25750) {
+          pos.z -= 128 * timeDelta / 20500;
+          this.el.setAttribute('position', pos);
+        } else if (state <= 36250) {
+          pos.x -= 40 * timeDelta / 10500;
+          this.el.setAttribute('position', pos);
+        } else if (state <= 56750) {
+          pos.z += 128 * timeDelta / 20500;
+          this.el.setAttribute('position', pos);
+        } else {
+          pos.x += 20 * timeDelta / 5250;
+          this.el.setAttribute('position', pos);
+        }
+      }
+    });
+  }
 }
